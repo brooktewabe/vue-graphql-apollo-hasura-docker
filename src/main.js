6a0be3fd7af823700a2a2ba2b83d3c46c6092963
brooktewabe <brooktewabe00@gmail.com>
2024-08-27 15:24:13 +0300

    grapgQL-hasura

--- src/main.js
+++ src/main.js
@@ -6,9 +6,10 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 import VueApollo from "vue-apollo";
 
 const httpLink = new HttpLink({
-  uri: "YOUR_HASURA_URL",
+  uri: "https://nearby-barnacle-92.hasura.app/v1/graphql",
   headers: {
-    "x-hasura-admin-secret": "YOUR_HASURA_SECRET",
+    "x-hasura-admin-secret":
+      "0N89apDMRhaTWQnVrP5yyarb5Dxa3TVpLBnWeSLB18bfQ7X4l5mg74Y5fKIyeOT7",
     // 'x-hasura-access-key': 'YOUR_ACCESS_KEY'    // Alternatively, use this header if required
   },
 });
