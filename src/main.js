0a96a685ef796e5d150955ec345de766ee35ce27
brooktewabe <brooktewabe00@gmail.com>
2024-08-27 11:33:12 +0300

    'graphQL-hasura'

--- src/main.js
+++ src/main.js
@@ -1,10 +1,33 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import VueApollo from "vue-apollo";
 
-Vue.config.productionTip = false;
+const httpLink = new HttpLink({
+  uri: "https://nearby-barnacle-92.hasura.app/v1/graphql",
+  headers: {
+    "x-hasura-admin-secret":
+      "0N89apDMRhaTWQnVrP5yyarb5Dxa3TVpLBnWeSLB18bfQ7X4l5mg74Y5fKIyeOT7",
+    // 'x-hasura-access-key': 'YOUR_ACCESS_KEY'    // Alternatively, use this header if required
+  },
+});
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+});
+
+Vue.use(VueApollo);
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+});
 
 new Vue({
-  router,
+  el: "#app",
+  apolloProvider,
   render: (h) => h(App),
-}).$mount("#app");
+});
